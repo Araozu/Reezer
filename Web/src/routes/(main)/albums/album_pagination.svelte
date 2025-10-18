628659f26e6aef3b00acb3aebcf4b3a20f05e6b6
Fernando Araoz <fernando@araozu.dev>
2025-10-18 07:21:37 -0500

    feat: improve album grid

--- Web/src/routes/(main)/albums/album_pagination.svelte
+++ Web/src/routes/(main)/albums/album_pagination.svelte
@@ -29,37 +29,39 @@
 	});
 </script>
 
-<Pagination.Root
-	bind:page={requestPage}
-	count={totalCount}
-	perPage={pageSize}
-	siblingCount={2}
->
-	{#snippet children({ pages, currentPage })}
-		<Pagination.Content>
-			<Pagination.Item>
-				<Pagination.PrevButton />
-			</Pagination.Item>
-			{#each pages as page (page.key)}
-				{#if page.type === "ellipsis"}
-					<Pagination.Item>
-						<Pagination.Ellipsis />
-					</Pagination.Item>
-				{:else}
-					<Pagination.Item>
-						<Pagination.Link
-							{page}
-							isActive={currentPage ===
-								page.value}
-						>
-							{page.value}
-						</Pagination.Link>
-					</Pagination.Item>
-				{/if}
-			{/each}
-			<Pagination.Item>
-				<Pagination.NextButton />
-			</Pagination.Item>
-		</Pagination.Content>
-	{/snippet}
-</Pagination.Root>
+<div class="py-4">
+	<Pagination.Root
+		bind:page={requestPage}
+		count={totalCount}
+		perPage={pageSize}
+		siblingCount={2}
+	>
+		{#snippet children({ pages, currentPage })}
+			<Pagination.Content>
+				<Pagination.Item>
+					<Pagination.PrevButton />
+				</Pagination.Item>
+				{#each pages as page (page.key)}
+					{#if page.type === "ellipsis"}
+						<Pagination.Item>
+							<Pagination.Ellipsis />
+						</Pagination.Item>
+					{:else}
+						<Pagination.Item>
+							<Pagination.Link
+								{page}
+								isActive={currentPage ===
+									page.value}
+							>
+								{page.value}
+							</Pagination.Link>
+						</Pagination.Item>
+					{/if}
+				{/each}
+				<Pagination.Item>
+					<Pagination.NextButton />
+				</Pagination.Item>
+			</Pagination.Content>
+		{/snippet}
+	</Pagination.Root>
+</div>
