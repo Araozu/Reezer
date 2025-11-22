3bddba388dfb774f9fddbbe437aeb935614c57ac
Fernando Araoz <fernando@araozu.dev>
2025-11-22 16:49:14 -0500

    feat: bump to .NET10 stable. add aspnet identity

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Identity;
 using Reezer.Api.Hubs;
 using Reezer.Application;
 using Reezer.Infrastructure;
+using Reezer.Infrastructure.Data;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +15,28 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplicationServices();
 builder.Services.AddSignalR();
 builder.Services.AddOpenApi();
+
+// Configure ASP.NET Core Identity
+builder
+    .Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+    {
+        // Password settings
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequiredLength = 8;
+
+        // Lockout settings
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+        options.Lockout.MaxFailedAccessAttempts = 5;
+        options.Lockout.AllowedForNewUsers = true;
+
+        // User settings
+        options.User.RequireUniqueEmail = true;
+    })
+    .AddEntityFrameworkStores<ReezerDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddCors(options =>
 {
@@ -39,6 +63,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors("Allow Frontend");
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Hubs
 app.MapHub<MusicHub>("api/hubs/music");
--- Infrastructure/Data/ReezerDbContext.cs
+++ Infrastructure/Data/ReezerDbContext.cs
@@ -1,15 +1,21 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Reezer.Domain.Entities;
 using Reezer.Domain.Entities.Songs;
 
 namespace Reezer.Infrastructure.Data;
 
-public class ReezerDbContext(DbContextOptions<ReezerDbContext> options) : DbContext(options)
+public class ReezerDbContext(DbContextOptions<ReezerDbContext> options)
+    : IdentityDbContext<IdentityUser>(options)
 {
     public DbSet<Song> Songs { get; set; }
     public DbSet<Artist> Artists { get; set; }
     public DbSet<Album> Albums { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ReezerDbContext).Assembly);
+    }
 }
