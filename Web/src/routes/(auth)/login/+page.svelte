068b027c6820cfa56e22eb9d5f78c82209d6966d
Fernando Araoz <fernando@araozu.dev>
2025-11-22 17:25:00 -0500

    feat: shadcn login form

---
+++ Web/src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import LoginForm from "$lib/components/login-form.svelte";
+	import { Disc3 } from "lucide-svelte";
+</script>
+
+<div
+	class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10"
+>
+	<div class="flex w-full max-w-sm flex-col gap-6">
+		<a
+			href="##"
+			class="flex items-center gap-2 self-center font-medium"
+		>
+			<div
+				class="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md"
+			>
+				<Disc3 class="size-4" />
+			</div>
+			Reezer
+		</a>
+		<LoginForm />
+	</div>
+</div>
