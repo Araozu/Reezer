cfd660f20d0e963a49fc3d0186f06a6fb587bf10
Fernando Araoz <fernando@araozu.dev>
2025-11-22 19:20:05 -0500

    feat: credentials for oauth

--- Web/src/routes/(auth)/login/+page.svelte
+++ Web/src/routes/(auth)/login/+page.svelte
@@ -3,21 +3,22 @@
 	import { Disc3 } from "lucide-svelte";
 </script>
 
+<svelte:head>
+	<title>Reezer - Login</title>
+</svelte:head>
+
 <div
 	class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10"
 >
 	<div class="flex w-full max-w-sm flex-col gap-6">
-		<a
-			href="##"
-			class="flex items-center gap-2 self-center font-medium"
-		>
+		<div class="flex items-center gap-2 self-center font-medium">
 			<div
 				class="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md"
 			>
 				<Disc3 class="size-4" />
 			</div>
 			Reezer
-		</a>
+		</div>
 		<LoginForm />
 	</div>
 </div>
