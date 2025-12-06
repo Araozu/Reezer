cc926c283efe6d28e3eb3cbec2c710516661382b
Fernando Araoz <fernando@araozu.dev>
2025-12-06 07:03:36 -0500

    feat: prepare for yt

--- Infrastructure/Options/StorageOptions.cs
+++ Infrastructure/Options/StorageOptions.cs
@@ -4,4 +4,5 @@ public class StorageOptions
 {
     public required string LibraryInitPath { get; set; }
     public required string LibraryTranscodedPath { get; set; }
+    public required string LibraryYtPath { get; set; }
 }
