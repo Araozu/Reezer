ccc15ad7a5dc4b9596bd4bd7d1bc58c7f19c2b1d
Fernando Araoz <fernando@araozu.dev>
2025-12-06 07:25:51 -0500

    feat: split folders for cached values

--- Infrastructure/Options/StorageOptions.cs
+++ Infrastructure/Options/StorageOptions.cs
@@ -5,4 +5,8 @@ public class StorageOptions
     public required string LibraryInitPath { get; set; }
     public required string LibraryTranscodedPath { get; set; }
     public required string LibraryYtPath { get; set; }
+
+    //
+    public required string AlbumCoverPath { get; set; }
+    public required string YtThumbnailPath { get; set; }
 }
